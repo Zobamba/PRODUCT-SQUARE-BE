91f1df693b9aa9009794d3682b88ba02aaffbc31
Onah Bernard Chizoba <onahbernardchizoba@gmail.com>
2024-02-04 13:49:02 +0100

    added fe link

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ dotenv.config();
 const app = express();
 
 const corsOptions = {
-  origin: ['http://localhost:3000'],
+  origin: ['http://localhost:3000', 'https://productsquare.netlify.app'],
   credentials: true,
 };
 
