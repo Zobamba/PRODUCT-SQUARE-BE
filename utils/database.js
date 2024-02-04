f17255a2a2328013992ad7222ec416daa3485e8f
Onah Bernard Chizoba <onahbernardchizoba@gmail.com>
2024-02-04 12:53:45 +0100

    db connection

--- utils/database.js
+++ utils/database.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const sequelize = new Sequelize(process.env.DB_URL, {
+const sequelize = new Sequelize({
   username: "onahbernardchizoba",
   password: "DhEbtkn39CwO",
   database: "p-square-db",
